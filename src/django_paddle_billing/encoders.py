abdad55798ee3f8d3d3478577c52ea9d130850bb
Benjamin Gervan <gben9302@gmail.com>
2024-01-23 19:20:05 +0100

    Package base
    Github workflows and helper packages

---
+++ src/django_paddle_billing/encoders.py
@@ -0,0 +1,6 @@
+from django.core.serializers.json import DjangoJSONEncoder
+
+
+class PrettyJSONEncoder(DjangoJSONEncoder):
+    def __init__(self, *args, indent, sort_keys, **kwargs):
+        super().__init__(*args, indent=4, sort_keys=True, **kwargs)
