c4d21ed6cccf6ef7bcdd3bbcd6bf0355b294bf64
Benjamin Gervan <gben9302@gmail.com>
2024-01-23 19:31:41 +0100

    Add example test

---
+++ tests/test_encoder.py
@@ -0,0 +1,27 @@
+import pytest
+import json
+from django_paddle_billing.encoders import PrettyJSONEncoder
+
+
+def test_pretty_json_encoder_produces_readable_output():
+    data = {"key": "value", "number": 123, "list": [1, 2, 3]}
+    encoder = PrettyJSONEncoder(indent=4, sort_keys=True)
+    encoded = encoder.encode(data)
+    expected = json.dumps(data, indent=4, sort_keys=True)
+    assert encoded == expected
+
+
+def test_pretty_json_encoder_handles_empty_input():
+    data = {}
+    encoder = PrettyJSONEncoder(indent=4, sort_keys=True)
+    encoded = encoder.encode(data)
+    expected = json.dumps(data, indent=4, sort_keys=True)
+    assert encoded == expected
+
+
+def test_pretty_json_encoder_handles_nested_input():
+    data = {"key": {"nested_key": "nested_value"}}
+    encoder = PrettyJSONEncoder(indent=4, sort_keys=True)
+    encoded = encoder.encode(data)
+    expected = json.dumps(data, indent=4, sort_keys=True)
+    assert encoded == expected
